f3bbffc2b244e02fe8aa758b5c6ee55b012629f3
n22044 <e1n22044@oit.c.jp>
2025-06-30 15:07:53 +0900

    p59: テストメソッドを複数追加

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -4,10 +4,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
+
     @Test
-    public void testAppReturnHelloWorld() {
-        App classUnderTest = new App();
-        // P56: GitHub Actions 起動テストのためにコメントを追加
-        assertEquals("P56 Test: Hello World expected", "Hello World!", classUnderTest.getGreeting());
+    public void testSum_TwoAndThree_ReturnsFive() {
+        Calculate calc = new Calculate();
+        assertEquals(5, calc.sum(2, 3));
+    }
+
+    @Test
+    public void testSum_MinusOneAndThree_ReturnsTwo() {
+        Calculate calc = new Calculate();
+        assertEquals(2, calc.sum(-1, 3));
+    }
+
+    @Test
+    public void testSum_ZeroAndZero_ReturnsZero() {
+        Calculate calc = new Calculate();
+        assertEquals(0, calc.sum(0, 0));
     }
 }
